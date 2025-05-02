c6dc74c79ca982df7562906c33a3e76f444efb03
redzhebmehmedd <recepcansf@gmail.com>
2025-05-02 13:42:11 +0200

    day08

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -1,5 +1,10 @@
 package utilities;
 
+import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReusableMethods {
 
     public static void bekle (int saniye){
@@ -10,4 +15,19 @@ public class ReusableMethods {
             throw new RuntimeException(e);
         }
     }
+
+    public static List<String>stringListesineDonustur(List<WebElement> webelementListesi) {
+
+        List<String> istenenStringList = new ArrayList<>();
+
+        for (WebElement eachElement : webelementListesi
+        ) {
+            istenenStringList.add(eachElement.getText());
+        }
+
+        return istenenStringList;
+
+    }
+
+
 }
